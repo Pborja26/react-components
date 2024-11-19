2b69739db63101b0fd882385a08ad3965a38e7a9
Pedro Borja <Pedro.borja@educat.com.br>
2024-11-19 17:07:36 -0300

    Initi progress bar

---
+++ src/components/Progress-Bar/ProgressBarTypes.ts
@@ -0,0 +1,9 @@
+export interface ProgressBarProps {
+  firstvalue?: number;
+  secondvalue?: number;
+  thirdvalue?: number;
+  fourthvalue?: number;
+  fifthvalue?: number;
+  showvalues?: boolean;
+  
+}
---
+++ src/utils/themes/Themes.ts
@@ -0,0 +1,28 @@
+export const Themes = {
+  primary: {
+    main: "",
+    light: "",
+    dark: ""
+  },
+  secondary: {
+    main: "",
+    light: "",
+    dark: ""
+  },
+  feedback: {
+    success: "",
+    warning: "",
+    info: "",
+    error: "",
+    delete: "",
+    disabled: "",
+    required: "",
+    close: ""
+  },
+  hover: {
+    primary: "",
+    secondary: "",
+    delete: "",
+    close: ""
+  }
+}
